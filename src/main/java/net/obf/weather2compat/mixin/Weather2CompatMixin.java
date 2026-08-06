076e721dd924dfe446c6167c1344757ad5ca1311
ninjaguardian <110734253+ninjaguardian@users.noreply.github.com>
2026-08-06 18:33:56 -0500

    Make rain checker detect if pos can see sky

--- src/main/java/net/obf/weather2compat/mixin/Weather2CompatMixin.java
+++ src/main/java/net/obf/weather2compat/mixin/Weather2CompatMixin.java
@@ -1,19 +1,23 @@
 package net.obf.weather2compat.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import weather2.ServerTickHandler;
 
 @Mixin(Level.class)
 public class Weather2CompatMixin {
-    /**
-     * @author ninjaguardian
-     * @reason Switched to Weather2's system
-     */
-    @Overwrite
-    public boolean isRainingAt(BlockPos pos) {
-        return ServerTickHandler.getWeatherManagerFor((Level) (Object) this).isPrecipitatingAt(pos);
+    @Redirect(
+            method = "isRainingAt",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;isRaining()Z"
+            )
+    )
+    private boolean isRaining(Level instance, @Local(argsOnly = true) BlockPos pos) {
+        return ServerTickHandler.getWeatherManagerFor(instance).isPrecipitatingAt(pos);
     }
 }
